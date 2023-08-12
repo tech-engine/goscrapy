54399f28db4c2e12937b5adeedb179dc1e8d9f77
imspyde <kumarnav.pradeep34@yahoo.com>
2023-08-12 19:08:21 +0530

    worked on goscrapy readme

---
+++ _examples/scrapethissite/scrapethissite/core.go
@@ -0,0 +1,54 @@
+package scrapeThisSite
+
+// do not modify this file
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/tech-engine/goscrapy/pkg/core"
+)
+
+type Spider struct {
+	baseUrl   *url.URL
+	ctx       context.Context
+	delegator core.Delegator[*Job, []Record]
+}
+
+func (s *Spider) NewJob(id string) *Job {
+	return NewJob(id)
+}
+
+func (s *Spider) Close() {
+}
+
+func (s *Spider) SetDelegator(delegator core.Delegator[*Job, []Record]) {
+	s.delegator = delegator
+}
+
+func (s *Spider) NewRequest() *core.Request {
+	return s.delegator.NewRequest()
+}
+
+func (s *Spider) Request(ctx context.Context, req *core.Request, cb core.ResponseCallback) {
+	s.delegator.ExRequest(ctx, req, cb)
+}
+
+func (s *Spider) yield(output *Output) {
+	s.delegator.Yield(output)
+}
+
+func (s *Spider) jobFromContext(ctx context.Context) (*Job, error) {
+	meta, ok := ctx.Value("META_DATA").(map[string]any)
+
+	if !ok {
+		return nil, ERR_EXTRACTING_META
+	}
+
+	job, ok := meta["JOB"].(*Job)
+
+	if !ok {
+		return nil, ERR_EXTRACTING_JOB
+	}
+	return job, nil
+}
