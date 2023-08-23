ab35dd38d3af10e5b17e4472526dbc65f0ae5f46
techengine <pankab37@gmail.com>
2023-08-24 00:41:39 +0530

    bugfix: data race in dupefilter_test.go

--- pkg/middlewares/dupefilter/dupefilter_test.go
+++ pkg/middlewares/dupefilter/dupefilter_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,9 +15,9 @@ import (
 type testCase struct {
 	Name,
 	Method string
-	Header  http.Header
-	Body    io.Reader
-	Blocked bool
+	Header   http.Header
+	Body     io.Reader
+	mayBlock bool
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +40,7 @@ func TestDupeFilter(t *testing.T) {
 			Header: http.Header{
 				"X-test": []string{"test_val_1"},
 			},
-			Body: nil,
+			mayBlock: true,
 		},
 		{
 			Name:   "test2",
@@ -55,8 +56,8 @@ func TestDupeFilter(t *testing.T) {
 			Header: http.Header{
 				"X-test": []string{"test_val_1"},
 			},
-			Body:    nil,
-			Blocked: true,
+			Body:     nil,
+			mayBlock: true,
 		},
 		{
 			Name:   "test4",
@@ -64,7 +65,8 @@ func TestDupeFilter(t *testing.T) {
 			Header: http.Header{
 				"X-test-another": []string{"test_val_1"},
 			},
-			Body: strings.NewReader("hello"),
+			Body:     strings.NewReader("hello"),
+			mayBlock: true,
 		},
 		{
 			Name:   "test5",
@@ -80,8 +82,8 @@ func TestDupeFilter(t *testing.T) {
 			Header: http.Header{
 				"X-test-another": []string{"test_val_1"},
 			},
-			Body:    strings.NewReader("hello"),
-			Blocked: true,
+			Body:     strings.NewReader("hello"),
+			mayBlock: true,
 		},
 		{
 			Name:   "test7",
@@ -96,12 +98,12 @@ func TestDupeFilter(t *testing.T) {
 	expectedPassCases := 5
 	actualPassCases := 0
 
+	var m sync.Mutex
 	for _, tc := range testCases {
 		func(tc testCase) {
 
 			t.Run(tc.Method, func(t *testing.T) {
 				t.Parallel()
-
 				req, err := http.NewRequest(tc.Method, testServer.URL, tc.Body)
 
 				assert.Nil(t, err, "error creating http request", tc.Name)
@@ -109,13 +111,14 @@ func TestDupeFilter(t *testing.T) {
 				req.Header = tc.Header
 
 				resp, err := client.Do(req)
-
-				if tc.Blocked && err != nil {
+				if tc.mayBlock && err != nil {
 					assert.ErrorIs(t, err, ERR_DUPEFILTER_BLOCKED, fmt.Sprintf("http request %s not blocked", tc.Name))
 				}
 
 				if resp != nil {
 					assert.Equal(t, 200, resp.StatusCode, "statuscode 200 expected")
+					m.Lock()
+					defer m.Unlock()
 					actualPassCases++
 				}
 
