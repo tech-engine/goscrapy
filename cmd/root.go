9002c4cc78e361df70cbfd985772047bf6efc902
techengine <pankab37@gmail.com>
2023-09-02 09:57:54 +0530

    added: setup.go creation via goscrapy command to assist easy setup

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.7.0"
+const VERSION = "0.7.5"
 
 const BANNER_MSG = `
 _________       ________                                         
