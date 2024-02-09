74b77e52dbc4ae916a93b3575a7fdec5b7502ed2
techengine <pankab37@gmail.com>
2024-02-09 15:12:43 +0530

    refactor: Export2JSON is now more flexible and test friendly

--- pkg/builtin/pipelines/export_to_json.go
+++ pkg/builtin/pipelines/export_to_json.go
@@ -2,7 +2,10 @@ package pipelines
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"os"
+	"time"
 
 	"github.com/tech-engine/goscrapy/pkg/core"
 	pm "github.com/tech-engine/goscrapy/pkg/pipeline_manager"
@@ -11,33 +14,28 @@ import (
 
 type export2JSON[OUT any] struct {
 	filename string
+	file     io.WriteCloser
 }
 
-func Export2JSON[OUT any](args ...string) *export2JSON[OUT] {
-	var filename string
-	if len(args) > 0 {
-		filename = args[0]
-	}
+func Export2JSON[OUT any]() *export2JSON[OUT] {
 	return &export2JSON[OUT]{
-		filename: filename,
+		filename: fmt.Sprintf("JOB_%s.json", time.Now().UTC().Format("2006-01-02-15-04-05")),
 	}
+}
+
+func (p *export2JSON[OUT]) WithWriteCloser(w io.WriteCloser) *export2JSON[OUT] {
+	p.file = w
+	return p
+}
+
+func (p *export2JSON[OUT]) WithFilename(n string) *export2JSON[OUT] {
+	p.filename = n
+	return p
 }
 
 func (p *export2JSON[OUT]) Open(ctx context.Context) error {
-	return nil
-}
-
-func (p *export2JSON[OUT]) Close() {
-}
-
-func (p *export2JSON[OUT]) ProcessItem(item pm.IPipelineItem, original core.IOutput[OUT]) error {
-
-	if original.IsEmpty() {
+	if p.file != nil {
 		return nil
-	}
-
-	if p.filename == "" {
-		p.filename = "JOB_" + original.Job().Id() + ".json"
 	}
 
 	file, err := os.OpenFile(p.filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
@@ -46,9 +44,21 @@ func (p *export2JSON[OUT]) ProcessItem(item pm.IPipelineItem, original core.IOut
 		return err
 	}
 
-	defer file.Close()
+	p.file = file
+	return nil
+}
 
-	jsonEncoder := json.NewEncoder(file)
+func (p *export2JSON[OUT]) Close() {
+	p.file.Close()
+}
+
+func (p *export2JSON[OUT]) ProcessItem(item pm.IPipelineItem, original core.IOutput[OUT]) error {
+
+	if original.IsEmpty() {
+		return nil
+	}
+
+	jsonEncoder := json.NewEncoder(p.file)
 
 	// Encode and write the JSON data
 	if err := jsonEncoder.Encode(original.Records()); err != nil {
