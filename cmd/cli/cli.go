b5f783e7158a8516d49ae46667bc772e1c1f6841
techengine <24850353+tech-engine@users.noreply.github.com>
2024-09-12 01:53:00 +0530

    update: goscrapy cli settings.tmpl template

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.11.0"
+const VERSION = "0.11.1"
 
 const BANNER_MSG = `
 _________       ________                                         
