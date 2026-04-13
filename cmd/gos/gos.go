882c1eba832483de96a8fa71b6511012afebe275
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-13 22:02:09 +0530

    refactor: gos app builder

--- cmd/gos/gos.go
+++ cmd/gos/gos.go
@@ -6,49 +6,54 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
-	"github.com/tech-engine/goscrapy/internal/types"
 	"github.com/tech-engine/goscrapy/pkg/core"
 	"github.com/tech-engine/goscrapy/pkg/engine"
 	"github.com/tech-engine/goscrapy/pkg/executor"
-	httpnative "github.com/tech-engine/goscrapy/pkg/executor_adapters/http_native"
+	httpAdapter "github.com/tech-engine/goscrapy/pkg/executor_adapters/http_native"
 	"github.com/tech-engine/goscrapy/pkg/logger"
 	"github.com/tech-engine/goscrapy/pkg/middlewaremanager"
 	pipelinemanager "github.com/tech-engine/goscrapy/pkg/pipeline_manager"
 	"github.com/tech-engine/goscrapy/pkg/scheduler"
 	ts "github.com/tech-engine/goscrapy/pkg/telemetry/stats"
+
 )
 
-func New[OUT any]() *gosBuilder[OUT] {
+func NewApp[OUT any]() *app[OUT] {
 
-	httpClient := &http.Client{}
+	httpClient := &http.Client{
+		Timeout:   30 * time.Second,
+		Transport: http.DefaultTransport.(*http.Transport).Clone(),
+	}
+
+	adapter := httpAdapter.NewAdapter(
+		httpAdapter.WithClient(httpClient),
+	)
+	executor := executor.New(adapter)
+	scheduler := scheduler.New(executor)
 	mm := middlewaremanager.New(httpClient)
 	pm := pipelinemanager.New[OUT]()
 
-	adapter := httpnative.NewHTTPClientAdapter(httpClient)
-	executor := executor.New(adapter)
-	scheduler := scheduler.New(executor)
+	eng := engine.New(scheduler, pm).
+		WithLogger(logger.NewLogger())
 
-	eng := engine.New(scheduler, pm)
-
-	//propagate logger to all components (recursive)
-	eng.WithLogger(logger.NewLogger())
-
-	builder := &gosBuilder[OUT]{
+	app := &app[OUT]{
+		Core:              core.New(eng),
 		Engine:            eng,
 		MiddlewareManager: mm,
 		PipelineManager:   pm,
 		Scheduler:         scheduler,
 	}
 
-	return builder
+	return app
 }
 
-func (gos *gosBuilder[OUT]) Setup(
+func (gos *app[OUT]) Setup(
 	middlewares []middlewaremanager.Middleware,
 	pipelines []pipelinemanager.IPipeline[OUT],
 	onShutdown ...func(),
-) *gosBuilder[OUT] {
+) *app[OUT] {
 	gos.logger.Infof("Initializing engine with %d middlewares and %d pipelines", len(middlewares), len(pipelines))
 	gos.MiddlewareManager.Add(middlewares...)
 	gos.PipelineManager.Add(pipelines...)
@@ -58,26 +63,44 @@ func (gos *gosBuilder[OUT]) Setup(
 	return gos
 }
 
-func (gos *gosBuilder[OUT]) WithStatsRecorderFactory(factory ts.IStatsRecorderFactory) *gosBuilder[OUT] {
+func (gos *app[OUT]) WithStatsRecorderFactory(factory ts.IStatsRecorderFactory) *app[OUT] {
 	if gos.Scheduler != nil && factory != nil {
 		gos.Scheduler.WithStatsRecorderFactory(factory)
 	}
 	return gos
 }
 
-func (gos *gosBuilder[OUT]) WithLogger(loggerIn core.IConfigurableLogger) *gosBuilder[OUT] {
+func (gos *app[OUT]) WithLogger(loggerIn core.IConfigurableLogger) *app[OUT] {
 	loggerIn = logger.EnsureLogger(loggerIn).(core.IConfigurableLogger)
 	gos.logger = loggerIn.WithName("GOS")
 	gos.Engine.WithLogger(loggerIn)
 	return gos
 }
 
-func (gos *gosBuilder[OUT]) Start(ctx context.Context) error {
+func (gos *app[OUT]) Telemetry() *ts.TelemetryHub {
+	if gos.hub == nil {
+		gos.hub = ts.NewTelemetryHub()
+	}
+	return gos.hub
+}
+
+func (gos *app[OUT]) Logger() core.ILogger {
+	return gos.logger
+}
+
+
+
+func (gos *app[OUT]) Start(ctx context.Context) error {
+	if gos.hub != nil {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		go gos.hub.Start(ctx)
+	}
 	return gos.Engine.Start(ctx)
 }
 
 // Wait for completion or termination
-func (gos *gosBuilder[OUT]) Wait(cancel context.CancelFunc, errCh <-chan error) error {
+func (gos *app[OUT]) Wait(cancel context.CancelFunc, errCh <-chan error) error {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
@@ -89,8 +112,4 @@ func (gos *gosBuilder[OUT]) Wait(cancel context.CancelFunc, errCh <-chan error) 
 		cancel()
 		return <-errCh
 	}
-}
-
-func NewBroadcaster(s ts.ISnapshotter, options ...types.OptFunc[ts.BroadcasterOpts]) *ts.Broadcaster {
-	return ts.NewBroadcaster(s, options...)
 }
