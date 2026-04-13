7d98892e4056faec362bd93b23c776d4a94b6b0b
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-13 05:37:33 +0530

    feat: add WithLogger method to scheduler and also refactor code.

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 	"sync"
 	"sync/atomic"
-	"time"
 
 	rp "github.com/tech-engine/goscrapy/internal/resource_pool"
 	"github.com/tech-engine/goscrapy/internal/types"
 	"github.com/tech-engine/goscrapy/pkg/core"
+	"github.com/tech-engine/goscrapy/pkg/engine"
 	"github.com/tech-engine/goscrapy/pkg/logger"
-	ts "github.com/tech-engine/goscrapy/pkg/telemetry/stats"
 )
 
 type scheduler struct {
@@ -26,7 +25,6 @@ type scheduler struct {
 	logger            core.ILogger
 }
 
-// NewScheduler creates a new scheduler.
 func New(executor IExecutor, optFuncs ...types.OptFunc[opts]) *scheduler {
 
 	// set default options
@@ -45,7 +43,7 @@ func New(executor IExecutor, optFuncs ...types.OptFunc[opts]) *scheduler {
 		responsePool:      rp.NewPooler(rp.WithSize[response](opts.reqResPoolSize)),
 		workerQueue:       make(WorkerQueue, opts.numWorkers),
 		workQueue:         make(WorkQueue, opts.workQueueSize),
-		logger:            logger.GetLogger(), // default to global logger
+		logger:            logger.EnsureLogger(nil).WithName("Scheduler"),
 	}
 }
 
@@ -53,83 +51,51 @@ func (s *scheduler) WithExecutor(executor IExecutor) {
 	s.executor = executor
 }
 
-func (s *scheduler) WithLogger(l core.ILogger) {
-	s.logger = l
-	s.executor.WithLogger(l)
+func (s *scheduler) WithLogger(loggerIn core.ILogger) engine.IScheduler {
+	loggerIn = logger.EnsureLogger(loggerIn)
+	s.logger = loggerIn.WithName("Scheduler")
+	s.executor.WithLogger(loggerIn)
+	return s
 }
 
-// Handles creating workers and listening on the work queue
 func (s *scheduler) Start(ctx context.Context) error {
-
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
+	s.logger.Infof("Starting scheduler with %d workers", s.opts.numWorkers)
 
 	var wg sync.WaitGroup
 
 	defer wg.Wait()
 	wg.Add(int(s.opts.numWorkers))
 
-	// worker lifecyle context
-	wCtx, wCancel := context.WithCancel(ctx)
+	for i := uint16(0); i < s.opts.numWorkers; i++ {
 
-	for i := uint16(1); i <= s.opts.numWorkers; i++ {
+		worker := NewWorker(i+1, s.executor, s.workerQueue, s.schedulerWorkPool, s.requestPool, s.responsePool)
+		worker.WithLogger(s.logger)
 		go func() {
 			defer wg.Done()
-
-			var recorder ts.StatRecorder
-			if s.opts.statsProducer != nil {
-				recorder = s.opts.statsProducer.NewWorkerCollector()
-			}
-
-			worker := NewWorker(i, s.executor, s.workerQueue, s.schedulerWorkPool, s.requestPool, s.responsePool, recorder)
-
-			// blocking
-			_ = worker.Start(wCtx)
+			worker.Start(ctx)
 		}()
 	}
 
-	// cancel worker context upon scheduler exit
-	defer wCancel()
-
-	for {
-		select {
-		case work := <-s.workQueue:
+	// scheduler loop
+	go func() {
+		for {
 			select {
-			case worker := <-s.workerQueue:
-				worker <- work
+
 			case <-ctx.Done():
-				// context cancellation from top(engine).
-				// we should try to put the work back or handle it.
-				// in graceful shutdown, we will handle this in the drain loop.
-				s.workQueue <- work
-				s.stopping.Store(true)
-				goto drain
-			}
-		case <-ctx.Done():
-			s.stopping.Store(true)
-			goto drain
-		}
-	}
 
-drain:
-	// draining the work queue
-	for {
-		select {
-		case work := <-s.workQueue:
-			select {
-			case worker := <-s.workerQueue:
+				s.stopping.Store(true)
+				return
+			case work := <-s.workQueue:
+				worker := <-s.workerQueue
 				worker <- work
-			case <-time.After(100 * time.Millisecond):
-				// if we can't get a worker within 100ms during drain,
-				// it might mean workers are stuck or busy.
-				s.workQueue <- work
+
 			}
-		default:
-			// queue is empty
-			return ctx.Err()
+
 		}
-	}
+	}()
+
+	s.logger.Infof("Scheduler stopped")
+	return nil
 }
 
 func (s *scheduler) Schedule(req core.IRequestReader, next core.ResponseCallback) {
