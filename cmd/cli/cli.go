7b50d40f09d31e5f0b66e14db4ee5a483ba633d6
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-15 20:39:51 +0530

    refactor: update cli to accomodate the ICoreSpider method name change for proper scalfolding.

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.13.3"
+const VERSION = "0.14.0"
 
 const BANNER_MSG = `
 _________       ________                                         
