5d1e6b5bb28be8ca5012903b2ab53ca5a6b07e9c
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-26 04:27:15 +0530

    refactor: update IPipelineManager, IScheduler added IWorkerPool, IResult, IScheduler, IPipeline and IPipelineItem interfaces in Engine

--- pkg/engine/ports.go
+++ pkg/engine/ports.go
@@ -9,22 +9,56 @@ import (
 type IPipelineManager[OUT any] interface {
 	Start(context.Context) error
 	Push(core.IOutput[OUT])
-	// WithLogger(core.ILogger) IPipelineManager[OUT]
-	// WithActivityTracker(core.IActivityTracker) IPipelineManager[OUT]
+	Add(...IPipeline[OUT])
+	Stop()
+}
+
+type IPipeline[OUT any] interface {
+	Open(context.Context) error
+	ProcessItem(IPipelineItem, core.IOutput[OUT]) error
+	Close()
+}
+
+type IPipelineItem interface {
+	Set(key string, value any) error
+	Get(key string) (any, bool)
+	Del(key string)
+	Clear()
+	Keys() []any
 }
 
 type Resetter interface {
 	Reset()
 }
 
+type IResult interface {
+	Request() *core.Request
+	Response() core.IResponseReader
+	CallbackName() string
+	TaskHandle() TaskHandle
+	Error() error
+	Release()
+}
+
+
+type TaskHandle = core.TaskHandle
+
+
 type IScheduler interface {
 	Start(context.Context) error
-	Schedule(*core.Request, string)
-	// Schedule(*core.Request, core.ResponseCallback)
-	NextRequest() (*core.Request, string, error)
-	// WithLogger(core.ILogger) IScheduler
-	// WithActivityTracker(core.IActivityTracker) IScheduler
+	Schedule(req *core.Request, cbName string) error
+	NextRequest(ctx context.Context) (*core.Request, string, TaskHandle, error)
+	Ack(handle TaskHandle) error
+	Nack(handle TaskHandle) error
 }
+
+type IWorkerPool interface {
+	Start(context.Context) error
+	Submit(req *core.Request, cbName string, handle TaskHandle) error
+	Results() <-chan IResult
+	ReleaseResult(IResult)
+}
+
 
 type ICallbackRegistry interface {
 	Register(string, core.ResponseCallback)
