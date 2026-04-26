6613f7519ed909f4fa38f1b275857d2e758e14d9
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-26 07:09:04 +0530

    refactor: remove the app type from gos types

--- pkg/gos/types.go
+++ pkg/gos/types.go
@@ -2,28 +2,7 @@ package gos
 
 import (
 	"context"
-	"net/http"
-
-	"github.com/tech-engine/goscrapy/pkg/core"
-	"github.com/tech-engine/goscrapy/pkg/engine"
-	ts "github.com/tech-engine/goscrapy/pkg/telemetry/stats"
-	"github.com/tech-engine/goscrapy/pkg/worker"
 )
-
-type app[OUT any] struct {
-	*core.Core[OUT]
-	Engine            core.IEngine[OUT]
-	PipelineManager   engine.IPipelineManager[OUT]
-	Scheduler         engine.IScheduler
-	WorkerPool        engine.IWorkerPool
-	Executor          worker.IExecutor
-	MiddlewareManager IMiddlewareManager
-	httpClient        *http.Client
-	logger            core.ILogger
-	hub               *ts.TelemetryHub
-	cancelableSignal  *cancelableSignal
-	lastErr           error
-}
 
 type cancelableSignal struct {
 	cancel context.CancelFunc
