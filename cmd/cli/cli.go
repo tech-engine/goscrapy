f9d27a1083fe0a4dadc18e7c27fbaba254006e70
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-28 13:05:05 +0530

    refactor: update settings.tmpl

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "0.15.1"
+const VERSION = "0.15.2"
 
 const BANNER_MSG = `
 _________       ________                                         
