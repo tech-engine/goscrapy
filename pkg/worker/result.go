5168098d7e57ee2c5dc775d96f4bd1106ed678dc
techengine <24850353+tech-engine@users.noreply.github.com>
2026-04-28 10:34:57 +0530

    feat: add reset method to result

--- pkg/worker/result.go
+++ pkg/worker/result.go
@@ -14,13 +14,22 @@ type result struct {
 	cancel       context.CancelFunc
 }
 
-func (r *result) Request() *core.Request      { return r.request }
+func (r *result) Request() *core.Request        { return r.request }
 func (r *result) Response() core.IResponseReader { return r.response }
-func (r *result) CallbackName() string        { return r.callbackName }
-func (r *result) TaskHandle() core.TaskHandle { return r.taskHandle }
-func (r *result) Error() error                { return r.err }
+func (r *result) CallbackName() string           { return r.callbackName }
+func (r *result) TaskHandle() core.TaskHandle    { return r.taskHandle }
+func (r *result) Error() error                   { return r.err }
 func (r *result) Release() {
 	if r.cancel != nil {
 		r.cancel()
 	}
+}
+
+func (r *result) Reset() {
+	r.request = nil
+	r.response = nil
+	r.callbackName = ""
+	r.taskHandle = nil
+	r.err = nil
+	r.cancel = nil
 }
