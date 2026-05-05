12acef98c77826c2ddd53b6b833b76e185e6b9eb
techengine <24850353+tech-engine@users.noreply.github.com>
2026-05-06 03:24:30 +0530

    refactor: keep rename identifiers in adaptive_test for consistency

--- pkg/worker/adaptive_test.go
+++ pkg/worker/adaptive_test.go
@@ -9,23 +9,23 @@ import (
 	"github.com/tech-engine/goscrapy/pkg/core"
 )
 
-type mockExecutor struct {
+type adaptiveTestExecutor struct {
 	execTime time.Duration
 }
 
-func (e *mockExecutor) Execute(req *core.Request, res core.IResponseWriter) error {
+func (e *adaptiveTestExecutor) Execute(req *core.Request, res core.IResponseWriter) error {
 	time.Sleep(e.execTime)
 	res.WriteStatusCode(200)
 	return nil
 }
 
-func TestAdaptiveScaling(t *testing.T) {
+func TestAdaptive_Scaling(t *testing.T) {
 	minWorkers := uint32(2)
 	maxWorkers := uint32(10)
 	execTime := 50 * time.Millisecond
 	window := 100 * time.Millisecond
 
-	executor := &mockExecutor{execTime: execTime}
+	executor := &adaptiveTestExecutor{execTime: execTime}
 
 	config := &Config{
 		Executor: executor,
@@ -38,22 +38,22 @@ func TestAdaptiveScaling(t *testing.T) {
 		},
 	}
 
-	p, err := NewPool(config)
+	wp, err := NewPool(config)
 	assert.NoError(t, err)
 
 	// we drain results to prevent blocking
 	go func() {
-		for range p.Results() {
+		for range wp.Results() {
 		}
 	}()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go p.Start(ctx)
+	go wp.Start(ctx)
 	time.Sleep(100 * time.Millisecond)
 
-	initialWorkers := p.(*workerPool).activeWorkers.Load()
+	initialWorkers := wp.(*workerPool).activeWorkers.Load()
 	assert.Equal(t, int32(minWorkers), initialWorkers)
 
 	// high load burst
@@ -64,7 +64,7 @@ func TestAdaptiveScaling(t *testing.T) {
 			case <-stopBurst:
 				return
 			default:
-				p.Submit(&core.Request{}, "cb", nil)
+				wp.Submit(&core.Request{}, "cb", nil)
 				time.Sleep(2 * time.Millisecond)
 			}
 		}
@@ -72,7 +72,7 @@ func TestAdaptiveScaling(t *testing.T) {
 
 	time.Sleep(2 * time.Second) // scaling window
 
-	burstWorkers := p.(*workerPool).activeWorkers.Load()
+	burstWorkers := wp.(*workerPool).activeWorkers.Load()
 	assert.Greater(t, burstWorkers, int32(minWorkers))
 	t.Logf("Scaled up to %d workers", burstWorkers)
 
@@ -82,7 +82,7 @@ func TestAdaptiveScaling(t *testing.T) {
 	// let's wait for EMA decay and worker exit
 	time.Sleep(8 * time.Second)
 
-	finalWorkers := p.(*workerPool).activeWorkers.Load()
+	finalWorkers := wp.(*workerPool).activeWorkers.Load()
 	assert.Equal(t, int32(minWorkers), finalWorkers)
 	t.Logf("Scaled down to %d workers", finalWorkers)
 }
